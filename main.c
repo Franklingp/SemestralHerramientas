fabc2271f5876206f5fdf013319cd52e7f767153
Franklin Pimentel <franklin.pimentel.1998@gmail.com>
2024-11-18 18:02:17 -0500

    Limpiar el codigo y colocando comentarios

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
+
+//funciones hello world
+void helloWorld(){
+    printf("Hello world \n");
+}
+
+//funcion principal main
 int main () {
 
-    printf("Hello bitch");
-    printf("franklin es marico");
+    //prueba de cooncepto
+    helloWorld();
+
     return 0;
 }
