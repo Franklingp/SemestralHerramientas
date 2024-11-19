4af615be3c9db1d637822afb48688cbc1b2e9f83
Franklin <franklin.pimentel.1998@gmail.com>
2024-11-19 09:02:59 -0500

    limpiando el codigo

--- main.c
+++ main.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//funciones hello world
-void helloWorld(){
-    printf("Hello world \n");
-}
+//variable global para saber cantidad de estudiantes
+int NUMERO_ESTUDIANTES=8;
 
-//funcion de suma
-int sumafunc(int numero, int numero2){
-
-
-   return 0;
+//Leer datos del archivo
+void leerDatosArchivo(){
+    printf("Lectura de datos \n");
 }
 
 //funcion principal main
 int main () {
 
     //defuincion de variables
-    int numero;
+
 
     //deficion de archivos 
 
     
     //prueba de cooncepto
-    helloWorld();
-    printf("Ingrese un numero: \n");
-    scanf(" %d", &numero);
-    numero ++;
-    printf("%d", numero);
+    printf("Inicio del semestral \n");
     return 0;
 }
