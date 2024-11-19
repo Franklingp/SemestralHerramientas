c46f6f6610ba3f027a550921867e71d74e80f498
Franklin <franklin.pimentel.1998@gmail.com>
2024-11-19 09:12:08 -0500

    definicion de estructuras

--- main.c
+++ main.c
@@ -4,6 +4,21 @@
 //variable global para saber cantidad de estudiantes
 int NUMERO_ESTUDIANTES=8;
 
+//estructura de registro de notas de estudiante
+struct registroEstudiante {
+    int cedula;
+    char nombre;
+
+};
+
+//estructura de registro de notas de estudiante
+struct registroNotas {
+    int año;
+    int semestre;
+    int codigoAsignacion[6];
+    char notas[6];
+};
+
 //Leer datos del archivo
 void leerDatosArchivo(){
     printf("Lectura de datos \n");
