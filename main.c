5fbc8c58650d66c160e5ccebc97c1585c8279041
Franklin <franklin.pimentel.1998@gmail.com>
2024-11-18 19:15:09 -0500

    Ejemplos de estructura

--- main.c
+++ main.c
@@ -6,9 +6,22 @@ void helloWorld(){
     printf("Hello world \n");
 }
 
+//funcion de suma
+int sumafunc(int numero, int numero2){
+
+
+   return 0;
+}
+
 //funcion principal main
 int main () {
+
+    //defuincion de variables
     int numero;
+
+    //deficion de archivos 
+
+    
     //prueba de cooncepto
     helloWorld();
     printf("Ingrese un numero: \n");
